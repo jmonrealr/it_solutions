f2b9bd29bf3bb1bd61bc71d5afebe93dc2b6114a
Josuepma <1930168@upv.edu.mx>
2022-03-28 09:17:06 -0600

    Creación del controlador del usuario

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = User::all();
+        return view('users',get_defined_vars());
+    }
+}
