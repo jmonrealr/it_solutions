0468229510c2cf4740e1056261f5020a7c9d6626
Crxwler <loco7139@gmail.com>
2022-03-29 23:37:44 -0600

    add: fontawesome

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,6 +21,10 @@
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+        <!-- FONTAWESOME -->
+        <script src="https://kit.fontawesome.com/a36cdd0297.js" crossorigin="anonymous"></script>
+
     </head>
 
 
@@ -43,6 +47,8 @@
                     <!--/app header-->
 						<!--Row-->
 						@yield('content')
+
+                    </div>
 				</div><!-- end app-content-->
 			</div>
 		</div>
