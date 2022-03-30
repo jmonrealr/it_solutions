1e610d289db8c56632ef04e4d1b6305282cc2241
Crxwler <loco7139@gmail.com>
2022-03-29 23:51:46 -0600

    add: Users Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,106 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Fortify\CreateNewUser;
+use App\Models\Department;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Password;
 
 class UserController extends Controller
 {
-    public function index(){
-        $users = User::all();
-        return view('users',get_defined_vars());
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+//        return view('users.index', ['users' => User::with('departments')->get()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+//        return view('users.create',['departments' => Department::all()]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $new_user = new CreateNewUser();
+        $user = $new_user->create($request->only(['name', 'email', 'password', 'password_confirmation']));
+        $user->deparments()->sync($request->departments);
+//        Password::sendResetLink($request->only(['email']));
+        $request->session()->flash('success', 'Usuario Creado');
+//        return redirect()->route('admin.users.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::with('departments')->findOrFail($id);
+//        return view('users.show', ['user' => $user]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.edit',
+            [
+                'user' => $user,
+                'departments'=> Department::all()
+            ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->except(['token', 'departments']));
+        $user->departments()->sync($request->departments);
+        $request->session()->flash('success', 'Usuario Editado');
+//        return redirect()->route('admin.users.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id, Request $request)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        $request->session()->flash('success', 'Usuario Eliminado');
+//        return redirect()->route('admin.users.index');
     }
 }
