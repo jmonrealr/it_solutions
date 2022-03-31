f545759bf6e37a777b36501b8097911063c8a1cc
Axel Issai Alemán Delgado <1930204@upv.edu.mx>
2022-03-30 18:48:13 -0600

    add: index view

--- resources/views/Proyectos/index.blade.php
+++ resources/views/Proyectos/index.blade.php
@@ -5,16 +5,17 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">TITULO DE LA VISTA AQUI</h4>
+        <h4 class="page-title">Proyectos</h4>
     </div>
     {{-- DESCOMENTAR DIV PARA MOSTRAR BOTON DE AGREGAR --}}
-    {{-- <div class="page-rightheader ml-md-auto">
+    <div class="page-rightheader ml-md-auto">
         <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
             <div class="btn-list">
-                <a href="#" class="btn btn-primary mr-3">
-                    Agregar cosa</a>
+                <a href="proyectos/create" class="btn btn-primary mr-3">
+                    Agregar proyecto</a>
             </div>
-        </div> --}}
+        </div>
+    </div>{{-- </div>   --}}
 </div>
 <!-- FIN CABECERA -->
 
@@ -24,10 +25,56 @@
 <!-- CONTENIDO -->
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
+        <div class="card">
+            <div class="card-header  border-0">
+                <h4 class="card-title">Lista de proyectos</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table  table-vcenter text-wrap table-bordered border-bottom" id="project-list">
+                        <thead>
+                            <tr>
+                                <th class="border-bottom-0">ID</th>
+                                <th class="border-bottom-0">Proyecto</th>
+                                <th class="border-bottom-0">Cliente</th>
+                                <th class="border-bottom-0">Fechas</th>
+                                <th class="border-bottom-0" Style="text-align: center;">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>01</td>
+                                <td><h6 class="mb-1 fs-14">Lorem ipsum dolor sit.</h6></td>
+                                <td>
+                                    <div class="d-flex">
+                                        <span class="avatar avatar-md brround mr-3" style="background-image: url({{asset('images/usuario.png')}}"></span>
+                                        <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                            <h6 class="mb-1 fs-14">Lorem ipsum dolor sit.</h6>
+                                            <p class="text-muted mb-0 fs-12">example@email.com</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="d-flex">
+                                        <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                            <h6 class="mb-1 fs-14"><span class="text-muted mb-0 fs-14">Inicio: </span>01/01/2022</h6>
+                                            <h6 class="mb-1 fs-14"><span class="text-muted mb-0 fs-14">Fin: </span>01/12/2022</h6>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td width='100px'>
+                                    <div class="d-flex">
+                                        <a href="empleados/ver" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Ver"><i class="fa-solid fa-eye text-primary"></i></a>
+                                        <a href="#" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Editar"><i class="fa-solid fa-pen text-success"></i></a>
+                                        <button class="action-btns1" data-toggle="tooltip" data-placement="top" title="Eliminar" type="submit"><i class="fa-regular fa-trash-can text-danger"></i></button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     
     </div>
 </div>
