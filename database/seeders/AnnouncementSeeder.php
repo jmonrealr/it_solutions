a2aa26486dec714afcef3d58f3143b0b475e1795
Josuepma <1930168@upv.edu.mx>
2022-04-02 08:17:49 -0600

    Seeder faltante de prueba

--- database/seeders/AnnouncementSeeder.php
+++ database/seeders/AnnouncementSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Announcement;
 
 class AnnouncementSeeder extends Seeder
 {
@@ -13,6 +14,6 @@ class AnnouncementSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Announcement::factory()->count(10)->create();
     }
 }
