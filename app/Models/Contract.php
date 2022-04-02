c6899e183bb76fe6a556e2d0093bbc45ff862d80
Crxwler <loco7139@gmail.com>
2022-04-02 11:22:52 -0600

    add: Contract model

---
+++ app/Models/Contract.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contract extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'subject',
+        'description',
+        'initiated_by',
+        'start_date',
+        'end_date',
+        'status_id',
+        'type_contract_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [];
+
+    //TODO: Relationships
+}
