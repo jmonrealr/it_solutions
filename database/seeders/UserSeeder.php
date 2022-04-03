8000263fffe0b1ebbb38ea9b768f8c28b7330478
Josuepma <1930168@upv.edu.mx>
2022-04-03 16:49:40 -0500

    Datos reales de usuarios con contraseñas

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,16 +15,16 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::factory()->create(
-            ['name' => 'Josue','email' => '1930168@gmail.com',]
+            ['name' => 'Josue','email' => '1930168@gmail.com','password' => 'josue']
         );
         User::factory()->create(
-            ['name' => 'Axel','email' => '1930204@gmail.com',]
+            ['name' => 'Axel','email' => '1930204@gmail.com','password' => 'axel']
         );
         User::factory()->create(
-            ['name' => 'Juan Carlos','email' => '1930345@gmail.com',]
+            ['name' => 'Juan Carlos','email' => '1930345@gmail.com','password' => 'juan']
         );
         User::factory()->create(
-            ['name' => 'Jordy','email' => '1930136@gmail.com',]
+            ['name' => 'Jordy','email' => '1930136@gmail.com','password' => 'jordy']
         );
                
         User::factory()->count(20)->create();
