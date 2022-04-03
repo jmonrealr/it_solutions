f231865eca66ea5d74f8076c42519aeefed77993
Josuepma <1930168@upv.edu.mx>
2022-04-03 17:48:16 -0500

    comentario

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        /**
+         * User es el modelo User,
+         * factory es la clase que usas para fabricar estos modelos, 
+         * count la cantidad de usuarios que agregas,
+         * create para especificar que vas a crear,
+         * Sequence lo utlizas para especificar una secuencia,
+         * Cada array representa uno de los usuarios que insertas
+         * al especificar los corchetes, reescribes los atributos que deseas
+         * con un arreglo asociativo.
+         * En el primero, se crea el usuario con el nombre Josue, email correspondiente
+         * y una contraseña, los demas campos los selecciona del factory
+         */  
         User::factory()->count(4)->create(
             new Sequence(
                 ['name' => 'Josue','email' => '1930168@gmail.com','password' => 'josue'],
