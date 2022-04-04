34406cce561ed8788aaad5ddf59435453100ea06
Josuepma <1930168@upv.edu.mx>
2022-04-04 18:36:04 -0500

    Campo de numero telefónico agregado

--- database/migrations/2022_03_30_041922_create_customers_table.php
+++ database/migrations/2022_03_30_041922_create_customers_table.php
@@ -17,6 +17,7 @@ class CreateCustomersTable extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('phone_number', 15)->nullable();
             $table->string('email');
             $table->timestamps();
         });
