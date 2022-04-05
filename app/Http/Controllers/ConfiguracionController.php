ebd97f45a66edd8de5de29d4dea31f6d777e35c6
Josuepma <1930168@upv.edu.mx>
2022-04-05 09:15:43 -0500

    Controlador para la configuración

---
+++ app/Http/Controllers/ConfiguracionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ConfiguracionController extends Controller
+{
+    /**
+     * 
+     */
+    public function index($id){
+        User::find($id)->get();
+        return view('Configuraciones.index',get_defined_vars());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Alert::success('Éxito', 'Empleado actualizado con éxito');
+        return redirect()->route('Configuraciones.index');
+    }
+}
