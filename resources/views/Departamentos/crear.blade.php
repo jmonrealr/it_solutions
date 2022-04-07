277ca73c4435442202cef8593b0fa7ec7e4790f0
Estefany-308 <1930611@upv.edu.mx>
2022-04-07 07:34:11 -0500

    se agrego vistas departamentos

--- resources/views/Departamentos/crear.blade.php
+++ resources/views/Departamentos/crear.blade.php
@@ -5,11 +5,11 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">TITULO DE LA VISTA AQUI</h4>
+        <h4 class="page-title">Agregar departamento</h4>
         <ul class="breadcrumb">
-            <li class="mb-1 fs-16"><a href="#">Nombre modulo</a></li>
+            <li class="mb-1 fs-16"><a href="#">Departamentos</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Agregar cosa</li>
+            <li class="text-muted mb-1 fs-16">Agregar departamento</li>
         </ul>
     </div>
 </div>
@@ -21,11 +21,49 @@
 <!-- CONTENIDO -->
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
+        <div class='row'>
+            <div class="col-xl-12 col-md-12 col-lg-12">
+                <div class="card">
+                    <form>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="form-label">Nombre</label>
+                                        <input class="form-control" type="text" placeholder="Ingresa el nombre del departamento" name="nombre" maxlength="22">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="form-label">Encargado</label>
+                                        <input class="form-control" type="text" placeholder="Ingresa nombre del encargado del departamento" name="apellidos" maxlength="30">
+                                    </div>
+                                </div>
+                            </div>
+                    
+                            <div class="row">
+                                <div class="col-md-10">
+                                    <div class="form-group">
+                                        <label class="form-label">Descripción</label>
+                                        <textarea class="form-control" type="text" rows="6" placeholder="Ingresa una descripción del funcionamiento del departamento" name="actividad"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer text-right">
+                                <a role="button" class="btn btn-outline-dark">
+                                    <i class="feather feather-corner-down-left sidemenu_icon"></i>
+                                    Cancelar
+                                </a>
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="feather  feather-save sidemenu_icon"></i>
+                                    Guardar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!-- FIN CONTENIDO -->
--- resources/views/Departamentos/index.blade.php
+++ resources/views/Departamentos/index.blade.php
@@ -5,16 +5,17 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">TITULO DE LA VISTA AQUI</h4>
+        <h4 class="page-title">Departamentos</h4>
     </div>
     {{-- DESCOMENTAR DIV PARA MOSTRAR BOTON DE AGREGAR --}}
-    {{-- <div class="page-rightheader ml-md-auto">
+    <div class="page-rightheader ml-md-auto">
         <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
             <div class="btn-list">
-                <a href="#" class="btn btn-primary mr-3">
-                    Agregar cosa</a>
+                <a href="departamentos/create" class="btn btn-primary mr-3">
+                    Agregar proyecto</a>
             </div>
-        </div> --}}
+        </div>
+    </div>
 </div>
 <!-- FIN CABECERA -->
 
@@ -24,12 +25,35 @@
 <!-- CONTENIDO -->
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group"><a class="btn btn-default btn-lg btn-space" role="button" style="width: 650px; height: 55px; background: none;" href="#">Departamento de dirección general</a>
+                            <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 650px; height: 55px; background: none;" href="#">Departamento de Finanzas</a>
+                            <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 650px; height: 55px; background: none;" href="#">Departamento de mercadotecnica</a>
+                            <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 650px; height: 55px; background: none;" href="#">Departamento de comunicaciones</a>
+                            <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 650px; height: 55px; background: none;" href="#">Departamento de gestión, planificación, y estrategía de servicio</a>
+                            <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 650px; height: 55px; background: none;" href="#">Departamento a atención a clientes y usuarios</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="btn btn-default btn-lg btn-space" role="button" style="width: 680px; height: 55px; background: none;" href="#">Departamento de riesgos</a>
+                        <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 680px; height: 55px; background: none;" href="#">Departamento de negocios y aplicaciones empresariales</a>
+                        <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 680px; height: 55px; background: none;" href="#">Departamento de desarrollo y nuevas tecnologías </a>
+                        <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 680px; height: 55px; background: none;" href="#">Departamento de sistemsas e infraestructura</a>
+                        <a class="btn btn-default btn-lg btn-space" role="button" style="margin-top: 40px; width: 680px; height: 55px; background: none;" href="#">Departamento de Recursos Humanos</a>
+                    </div>
+                </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
+
 </div>
 <!-- FIN CONTENIDO -->
 @endsection
