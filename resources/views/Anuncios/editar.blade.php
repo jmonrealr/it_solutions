4fa9fc9acee66136c0b738c63d472a4273e71290
JuanNava897 <1930536@upv.edu.mx>
2022-04-12 10:45:00 -0500

    vistas anuncios/ver/crear/editar terminaddas

--- resources/views/Anuncios/editar.blade.php
+++ resources/views/Anuncios/editar.blade.php
@@ -7,9 +7,9 @@
     <div class="page-leftheader">
         <h4 class="page-title">Editar anuncio</h4>
         <ul class="breadcrumb">
-            <li class="mb-1 fs-16"><a href="{{ url()->previous() }}">Nombre modulo</a></li>
+            <li class="mb-1 fs-16"><a href="{{ url()->previous() }}">Anuncios</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Editar cosa</li>
+            <li class="text-muted mb-1 fs-16">Editar anuncios</li>
         </ul>
     </div>
 </div>
@@ -19,14 +19,63 @@
 
 
 <!-- CONTENIDO -->
+
 <div class="row">
-    <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
-    </div>
+	<div class="col-xl-12 col-md-12 col-lg-12">
+		<div class="card">
+			<form action="{{route('anuncios.store')}}" method="POST" enctype="multipart/form-data">
+			@csrf
+			<div class="card-body">
+
+				<h4 class="mb-5 font-weight-semibold">Crea un anuncio</h4>
+
+
+				<div class="row">
+					<div class="col-md-1">
+						<div class="form-group">
+							<label class="form-label">ID</label>
+							<input class="form-control" type="text"  maxlength="22">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label class="form-label">Numero de vistas</label>
+							<input class="form-control" type="text"  maxlength="30">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label class="form-label">Contenido</label>
+							<textarea class="form-control" type="text" rows="6" placeholder="Ingresa el contenido de tu anuncio" name="actividad"></textarea>
+						</div>
+					</div>
+				</div>
+
+                <div class="row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label">Plataforma de emisión</label>
+							<input class="form-control" type="text" placeholder="facebook" name="plataforma" maxlength="22">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="card-footer text-right">
+				<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
+					<i class="feather feather-corner-down-left sidemenu_icon"></i>
+					Regresar
+				</a>
+				<button type="submit" class="btn btn-primary">
+					<i class="feather  feather-save sidemenu_icon"></i>
+					Guardar</button>
+			</div>
+		</form>
+		</div>
+	</div>
 </div>
 <!-- FIN CONTENIDO -->
 @endsection
--- resources/views/Anuncios/ver.blade.php
+++ resources/views/Anuncios/ver.blade.php
@@ -7,9 +7,9 @@
     <div class="page-leftheader">
         <h4 class="page-title">Ver anuncio</h4>
         <ul class="breadcrumb">
-            <li class="mb-1 fs-16"><a href="{{ url()->previous() }}">Nombre modulo</a></li>
+            <li class="mb-1 fs-16"><a href="{{ url()->previous() }}">Anuncios</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Ver cosa</li>
+            <li class="text-muted mb-1 fs-16">Ver anuncio</li>
         </ul>
     </div>
 </div>
@@ -19,14 +19,58 @@
 
 
 <!-- CONTENIDO -->
+
 <div class="row">
-    <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
-    </div>
+	<div class="col-xl-12 col-md-12 col-lg-12">
+		<div class="card">
+
+			<div class="card-body">
+
+				<h4 class="mb-5 font-weight-semibold">Anuncio del empleado</h4>
+
+
+				<div class="row">
+					<div class="col-md-1">
+						<div class="form-group">
+							<label class="form-label">ID</label>
+							<div class="text-muted">1930536</div>
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label class="form-label">Numero de vistas</label>
+							<div class="text-muted">23</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label class="form-label">Contenido</label>
+							<div class="text-muted">En esta semana se suspenden labores debido al decreto del gobernador</div>
+						</div>
+					</div>
+				</div>
+
+                <div class="row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label">Plataforma de emisión</label>
+							<div class="text-muted">Facebook</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="card-footer text-right">
+				<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
+					<i class="feather feather-corner-down-left sidemenu_icon"></i>
+					Regresar
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
 <!-- FIN CONTENIDO -->
 @endsection
