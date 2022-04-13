0daa8a7354c7c646604c17de29f60935afdc4ea1
aldopuga01 <56364832+aldopuga01@users.noreply.github.com>
2022-04-12 22:54:10 -0500

    Vistas actualizadas, crear, editar y borrar.

--- resources/views/Eventos/editar.blade.php
+++ resources/views/Eventos/editar.blade.php
@@ -5,11 +5,11 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">TITULO DE LA VISTA AQUI</h4>
+        <h4 class="page-title">Editar evento</h4>
         <ul class="breadcrumb">
-            <li class="mb-1 fs-16"><a href="{{ url()->previous() }}">Nombre modulo</a></li>
+            <li class="mb-1 fs-16"><a href="{{ url()->previous() }}">Eventos</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Agregar cosa</li>
+            <li class="text-muted mb-1 fs-16">Editar eventos</li>
         </ul>
     </div>
 </div>
@@ -20,13 +20,59 @@
 
 <!-- CONTENIDO -->
 <div class="row">
-    <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
-    </div>
-</div>
+            <div class="col-xl-12 col-md-12 col-lg-12">
+                <div class="card">
+                <form action="{{route('eventos.update',$eventos->id)}}" method = "POST">
+                    @method('PUT')
+                    @csrf
+                    <div class="card-body">
+                        <h4 class="mb-5 font-weight-semibold">Información del evento</h4>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label class="form-label">Nombre del evento</label>
+                                    <textarea class="form-control" type="text" placeholder="Ingresa el nombre evento" name="name" maxlength="22">{{$eventos->name}}</textarea>
+                                </div>
+                            </div>
+						</div>
+                        <div class="row">                         
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label class="form-label" for="start">Fecha</label>
+                                    <input class="form-control" type="date" name="start_date" id="start" value="{{$eventos->start_date}}" min="2016-01-01" max="2030-12-31">
+                                </div>
+                            </div>
+                            {{-- Descripción --}}
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="form-label">Descripción:</label>
+                                    <textarea rows="3" class="form-control" name="description" placeholder="Agrega una breve descripción" maxlength="250">{{$eventos->description}}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            {{-- solucion --}}
+                            <div class="col-md-10">
+                                <div class="form-group">
+                                    <label class="form-label">Solución del riesgo:</label>
+                                    <textarea rows="3" class="form-control" name="location" placeholder="Agrega una breve descripción" maxlength="250">{{$eventos->location}}</textarea>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="card-footer text-right">
+                        <a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
+                            <i class="feather feather-corner-down-left sidemenu_icon"></i>
+                            Regresar
+                        </a>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="feather  feather-save sidemenu_icon"></i>
+                            Guardar</button>
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
 <!-- FIN CONTENIDO -->
 @endsection
--- resources/views/Eventos/index.blade.php
+++ resources/views/Eventos/index.blade.php
@@ -5,16 +5,16 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">TITULO DE LA VISTA AQUI</h4>
+        <h4 class="page-title">Eventos</h4>
     </div>
-    {{-- DESCOMENTAR DIV PARA MOSTRAR BOTON DE AGREGAR --}}
-    {{-- <div class="page-rightheader ml-md-auto">
+    <div class="page-rightheader ml-md-auto">
         <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
             <div class="btn-list">
-                <a href="#" class="btn btn-primary mr-3">
-                    Agregar cosa</a>
+                <a href="{{route('eventos.create')}}" class="btn btn-primary mr-3">
+                    Agregar evento</a>
             </div>
-        </div> --}}
+        </div>
+    </div>    
 </div>
 <!-- FIN CABECERA -->
 
@@ -24,11 +24,63 @@
 <!-- CONTENIDO -->
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table  table-vcenter text-wrap table-bordered border-bottom dt" id="project-list">
+                        <thead>
+                            <tr>
+                                <th class="border-bottom-0">ID</th>
+                                <th class="border-bottom-0">Nombre</th>
+                                <th class="border-bottom-0">Descripción</th>
+                                <th class="border-bottom-0">Dirección</th>
+                                <th class="border-bottom-0">Fecha</th>
+                                <th class="border-bottom-0" Style="text-align: center;">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($eventos as $evento)
+                            <tr>    
+                                <td>{{$evento->id}}</td>
+                                <td>
+                                    <div class="d-flex">
+                                        <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                            <h6 class="mb-1 fs-14">{{$evento->name}}</h6>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="d-flex">
+                                        <div class="mr-3 mt-0 mt-sm-1 d-block">
+                                            <h6 class="mb-1 fs-14">{{$evento->description}}</h6>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span>
+                                        {{$evento->location}}
+                                    </span>
+                                </td>
+                                <td>
+                                    <span>{{$evento->start_date}}</span>
+                                </td>
+                                <td>
+                                <div class="d-flex">
+                                        <a href="{{route('eventos.edit',$evento->id)}}" class="action-btns1" data-toggle="tooltip" data-placement="top" title="Editar"><i class="fas fa-pen text-success"></i></a>
+                                        <form action="{{route('eventos.destroy', $evento->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="action-btns1" onclick="mensaje()" data-toggle="tooltip" data-placement="top" title="Eliminar" type="submit"><i class="fa fa-trash-o text-danger"></i></button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>        
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 <!-- FIN CONTENIDO -->
