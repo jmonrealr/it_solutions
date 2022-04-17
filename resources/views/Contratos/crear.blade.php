2859888ceeadfb08bcbd88957c9fda01eeb3da59
Jordy Lagunas Higuera <1930136@upv.edu.mx>
2022-04-16 20:53:06 -0500

    contratos

--- resources/views/Contratos/crear.blade.php
+++ resources/views/Contratos/crear.blade.php
@@ -5,11 +5,11 @@
 <!-- CABECERA -->
 <div class="page-header d-xl-flex d-block">
     <div class="page-leftheader">
-        <h4 class="page-title">TITULO DE LA VISTA AQUI</h4>
+        <h4 class="page-title">Agregar Contrato</h4>
         <ul class="breadcrumb">
-            <li class="mb-1 fs-16"><a href="#">Nombre modulo</a></li>
+            <li class="mb-1 fs-16"><a href="#">Contratos</a></li>
             <li class="text-muted mb-1 fs-16 ml-2 mr-2"> / </li>
-            <li class="text-muted mb-1 fs-16">Agregar cosa</li>
+            <li class="text-muted mb-1 fs-16">Agregar contrato</li>
         </ul>
     </div>
 </div>
@@ -20,13 +20,84 @@
 
 <!-- CONTENIDO -->
 <div class="row">
-    <div class="col-xl-12 col-md-12 col-lg-12">
-        
-        {{-- 
-            AQUI VA EL CONTENIDO DE LA VISTA 
-        --}}
-    
-    </div>
+<div class="col-xl-12 col-md-12 col-lg-12">
+		<div class="card">
+			<form>
+			<div class="card-body">
+				<h4 class="mb-5 font-weight-semibold">Información del contrato</h4>
+
+				<div class="row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label">Asunto</label>
+							<input class="form-control" type="text" placeholder="Ingresa el asunto" name="asunto" maxlength="22">
+						</div>
+					</div>
+					<div class="col-md-4">
+                        <div class="form-group">
+							<label class="form-label">Descripcion</label>
+							<textarea class="form-control" type="text" rows="6" placeholder="Ingrese una breve descripcion del contrato" name="descripcion"></textarea>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label">Iniciado por</label>
+							<input class="form-control" type="text" placeholder="Ingresa quien inicio el contrato" name="iniciado_por" maxlength="30">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-2">
+						<div class="form-group">
+							<label class="form-label">Fecha de inicio</label>
+							<input class="date" type="date" id="start" name="inicio"
+                                value="2018-07-22"
+                                min="2022-01-01" max="2040-12-31">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label class="form-label">Fecha de finalizacion</label>
+							<input class="date" data-date-format="mm/dd/yyyy" type="date" id="start" name="finalizacion"
+                                value="2018-07-22"
+                                min="2022-01-01" max="2040-12-31">
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label">Estado</label>
+							<input class="form-control" type="text" placeholder="Ingresa su cargo" name="estado" maxlength="16">
+						</div>
+					</div>
+
+                    <div class="col-md-4">
+                        <div class="form-group">
+							<label class="form-label">Tipo de contrato</label>
+							<select name="contrato" class="form-control custom-select select2" data-placeholder="Selecciona contrato">
+								<option label="Selecciona contrato"></option>
+								<option value="1">Temporal</option>
+								<option value="2">A Largo plazo</option>
+								<option value="2">Periodo de prueba</option>
+							</select>
+						</div>
+                     </div>
+				</div>
+
+
+			</div>
+			<div class="card-footer text-right">
+				<a role="button" class="btn btn-outline-dark" href="{{ url()->previous() }}">
+					<i class="feather feather-corner-down-left sidemenu_icon"></i>
+					Regresar
+				</a>
+				<button type="submit" class="btn btn-primary">
+					<i class="feather  feather-save sidemenu_icon"></i>
+					Guardar</button>
+			</div>
+		</form>
+		</div>
+	</div>
 </div>
 <!-- FIN CONTENIDO -->
 @endsection
--- resources/views/Contratos/index.blade.php
+++ resources/views/Contratos/index.blade.php
@@ -8,13 +8,14 @@
         <h4 class="page-title">Contratos</h4>
     </div>
     {{-- DESCOMENTAR DIV PARA MOSTRAR BOTON DE AGREGAR --}}
-    {{-- <div class="page-rightheader ml-md-auto">
+    <div class="page-rightheader ml-md-auto">
         <div class="align-items-end flex-wrap my-auto right-content breadcrumb-right">
             <div class="btn-list">
-                <a href="#" class="btn btn-primary mr-3">
-                    Agregar cosa</a>
+                <a href="contratos/create" class="btn btn-primary mr-3">
+                    <i class="feather  feather-plus sidemenu_icon"></i>Agregar Contrato</a>
             </div>
-        </div> --}}
+        </div>
+    </div>
 </div>
 <!-- FIN CABECERA -->
 
@@ -24,52 +25,77 @@
 <!-- CONTENIDO -->
 <div class="row">
     <div class="col-xl-12 col-md-12 col-lg-12">
-    <div class="card">
+        <div class="card">
             <div class="card-header  border-0">
-                {{--<h4 class="card-title">Lista de empleados</h4>--}}
+                <h4 class="card-title">Lista de Contratos</h4>
             </div>
             <div class="card-body">
-                <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
+                <div class="table-responsive">
+                    <table class="table  table-vcenter text-wrap table-bordered border-bottom dt" id="project-list">
+                        <thead>
+                            <tr>
+                                <th class="border-bottom-0">ID</th>
+                                <th class="border-bottom-0">Asunto</th>
+                                <th class="border-bottom-0">Descripcion</th>
+                                <th class="border-bottom-0">Iniciado por</th>
+                                <th class="border-bottom-0">Fecha de inicio</th>
+                                <th class="border-bottom-0">Fecha de finalizacion</th>
+                                <th class="border-bottom-0">Estado</th>
+                                <th class="border-bottom-0">Tipo de contrato</th>
+                                <th class="border-bottom-0" Style="text-align: center;">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+
+                    </table>
                 </div>
-                <br>
-                <div class="row">
-                <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                    <div class="col">
-                        <button class="btn-primary">Prueb</button>
-                    </div>
-                </div>
-                </div>
-                
             </div>
         </div>
     </div>
 </div>
 <!-- FIN CONTENIDO -->
+@endsection
+
+
+@section('extra-script')
+<script type="text/javascript">
+    function mensaje(){
+        const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-outline-dark'
+          },
+          buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+          title: '¿Eliminar Contrato?',
+          text: "Si eliminas este contrato se eliminará permanentemente del sistema.",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Si, eliminar Contrato!.',
+          cancelButtonText: 'No, mantener Contrato!.',
+          reverseButtons: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+              'Eliminado!',
+              'El Contrato se ha eliminado correctamente.',
+              'success'
+            )
+          } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            swalWithBootstrapButtons.fire(
+              'Proceso cancelado.',
+              '',
+              'error'
+            )
+          }
+        })
+    }
+</script>
 @endsection
